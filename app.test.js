701ae4c57d0d838e0d37574d0fda12439854e18a
Stanislav <119166926+StanislavSugak@users.noreply.github.com>
2025-05-04 14:04:08 +0300

    init: start work

---
+++ app.test.js
@@ -0,0 +1,13 @@
+const add = require('./app');
+
+test('adds 2 + 3 to equal 5', () => {
+    expect(add(2, 3)).toBe(5);
+});
+
+test('adds -1 + 1 to equal 0', () => {
+    expect(add(-1, 1)).toBe(0);
+});
+
+test('adds -2 + 3 to equal 1', () => {
+    expect(add(-2, 3)).toBe(1);
+});
